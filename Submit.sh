a00603143d8000731ec54d0f8f5ad36b23178b53
Sifu Luo <sifuluo@tamu.edu>
2020-02-07 00:59:55 +0100

    Batch running on lxplus testing

---
+++ Submit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd /afs/cern.ch/work/s/siluo/wprime/
+
+ID=$(( $2 + 1 ))
+
+echo "Job SampleType: $1 - Sample Number: $ID has begun..."
+
+root -l -b "GenMinimizeAllPermTest.cc+($1,$ID,-2)"
+
+echo "Job SampleType: $1 - Sample Number: $ID is done..."
